c9c0e8a408deef55f70e8b6ee56c4be951bd615d
SamuelBriggs <tolulope.alawode@gmail.com>
2024-07-07 21:08:21 +0100

    docker

--- src/test/java/com/example/stagetwohng/StageTwoHngApplicationTests.java
+++ src/test/java/com/example/stagetwohng/StageTwoHngApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.stagetwohng;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class StageTwoHngApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.stagetwohng;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class StageTwoHngApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}

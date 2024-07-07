c5bc63ed9e7c5af03296b4a8f8c67c4fdbbd5271
the_zen <alayandezainab64@gmail.com>
2024-07-07 19:30:17 +0100

    commit

--- src/main/java/com/example/stagetwohng/model/Organization.java
+++ src/main/java/com/example/stagetwohng/model/Organization.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class Organization {
 
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(unique = true)
